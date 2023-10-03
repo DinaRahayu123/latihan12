9689c31056fcf12b7623084399fb282fc461ed3c
rahayudina190@gmail.com <rahayudina190@gmail.com>
2023-10-03 16:47:28 +0700

    first commit

---
+++ resources/views/home.blade.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Home</title>
+    </head>
+    <body>
+
+        <!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>home</title>
+</head>
+<body>
+    <h1>Garuda Cyber Institute</h1>
+    <h2>Jadilah Programmer Handal Bersama GC-INS</h2>
+    <a href="/register">Register</a>
+    <p>Grow Together width Garuda Cyber Institute</p>
+   
+    <h2>Syarat Dan Ketentuan</h2>
+
+    <ul>
+        <li>Tamatan SMA/SMK</li>
+        <li>Tamatan Perguruan Tinggi</li>
+        <li>Pekerja IT</li>
+        <li>Freelancer</li>
+    </ul>
+
+    <h2>Cara Bergabung</h2>
+    <ul>
+        <li>Kunjungan website GC-INS</li>
+        <li>Register</li>
+        <li>Lakukan Pembayaran</li>
+    </ul>
+
+
+    <form action="/action page.HTML">
+        
+</body>
+</html>
+
+        <h1>Welcome to Home Page</h1>
+        <a href="/register">Register</a>
+    </body>
+</html>
